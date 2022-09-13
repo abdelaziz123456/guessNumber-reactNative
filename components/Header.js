82f3a5cb3b1c506cce0e514c214c3470987d839f
abdelaziz12121 <abdelazizmohamed12121@axlr8.tech>
2022-09-13 16:34:59 +0200

    add number input and container component

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-
+import colors from '../constants/colors'
 export default function Header(props) {
   return (
   <View style={styles.header}>
@@ -13,9 +13,9 @@ export default function Header(props) {
 const styles=StyleSheet.create({
 header:{
     width:'100%',
-    height:90,
-    paddingTop:36,
-    backgroundColor:'#f7287b',
+    height:80,
+    paddingTop:20,
+    backgroundColor:colors.primary,
     alignItems:'center',
     justifyContent:'center'
 },
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,21 +1,99 @@
-import React from "react";
-import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert,
+} from "react-native";
+import Card from "../components/Card";
+import Input from "../components/Input";
+import colors from "../constants/colors";
 
 export default function StartGameScreen() {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedNum, setSelectedNumber] = useState();
+  function inputHandler(inputText) {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  }
+
+  function resetInput() {
+    setEnteredValue("");
+    setConfirmed(false);
+  }
+
+  function confirmInput() {
+    const chosenNumber = parseInt(enteredValue);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert("Invalid Number", "Number should be  0 < N < 100 ", [
+        { text: "Okay", style: "destructive", onPress: resetInput },
+      ]);
+    }
+    setConfirmed(true);
+    setSelectedNumber(chosenNumber);
+    setEnteredValue("");
+  }
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start a New Game ! </Text>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a New Game ! </Text>
 
-      <View style={styles.inputContainer}>
-        <Text>Select A Number</Text>
-        <TextInput />
-        <View style={styles.buttonContainer}>
-          <Button title="Reset" onPress={() => {}} />
+        <Card style={styles.inputContainer}>
+          <Text>Select A Number</Text>
+          <Input
+            blurOnSubmit
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="numeric"
+            maxLength={2}
+            style={styles.input}
+            value={enteredValue}
+            onChangeText={inputHandler}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button
+                title="Reset"
+                onPress={resetInput}
+                color={colors.accent}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title="Confirm"
+                onPress={confirmInput}
+                color={colors.primary}
+              />
+            </View>
+          </View>
+        </Card>
+        {/* mssage fof chosen number */}
+        {confirmed ? (
+          <Card style={styles.chosenNumber}>
+            <Text style={{ textAlign: "center" }}>You Selected</Text>
+            <View style={styles.numContainer}>
+              <Text style={{ textAlign: "center", color: colors.accent }}>
+                {selectedNum}
+              </Text>
+            </View>
 
-          <Button title="Confirm" onPress={() => {}} />
-        </View>
+            <Button title='Start Game' color={colors.primary} />
+
+            
+          </Card>
+        ) : (
+          <></>
+        )}
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -25,20 +103,35 @@ const styles = StyleSheet.create({
     padding: 10,
     alignItems: "center",
   },
-  title:{
-fontSize:20,
-marginVertical:10,
-
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
   },
   buttonContainer: {
-    flexDirection:'row',
-    width:'100%',
-    justifyContent:'space-between',
-    paddingHorizontal:15
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
   },
-  inputContainer:{
-width:300,
-maxWidth:'80%',
-alignItems:'center'
-  }
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+  button: {
+    width: 80,
+  },
+  input: {
+    textAlign: "center",
+  },
+  chosenNumber: {
+    marginVertical: 20,
+  },
+  numContainer: {
+    marginVertical: 10,
+    borderColor: colors.accent,
+    borderWidth: 1,
+    padding:8,
+    borderRadius:9
+  },
 });
